7aaec74ae8a23420f43dd7dc9e0ea279ccb55016
Emil Torp <torp.emil@gmail.com>
2017-09-17 20:27:13 +0200

    Test of Travis instead of appveyor

--- build.sh
+++ build.sh
@@ -1,18 +1,11 @@
+## build.sh
+
 # Restore packages
 npm install
 dotnet restore
-
-# Delete old publish folder
-rm -rf ./Publish
 
 # Create new publish folder
 dotnet publish -c Release -o ./Publish
 
 # Build docker image
 docker build -t beermecore .
-
-# Tag image
-docker tag beermecore jpinc/beerme
-
-# Push image
-docker push jpinc/beerme
---
+++ deploy.sh
@@ -0,0 +1,7 @@
+## deploy.sh
+
+# Tag image
+docker tag beermecore jpinc/beerme
+
+# Push image
+docker push jpinc/beerme
